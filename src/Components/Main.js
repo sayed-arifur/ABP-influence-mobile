cae88f517e2b755eaca05b88a6648ea531e446d0
Arifur Rahaman <sayed.rahman@abpdigital.in>
2023-07-28 12:22:43 +0530

    green section

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,6 +8,14 @@ import banner2 from '../Components/assets/images/banner-2.jpg';
 import banner3 from '../Components/assets/images/banner-3.jpg';
 import abpInfluenceLogo from '../Components/assets/images/abp-influence-logo.png';
 import bannerGrid from '../Components/assets/images/bannerGrid.png';
+import secureEdge01 from '../Components/assets/images/01-secureEdge.svg';
+import secureEdge02 from '../Components/assets/images/02-secureEdge.svg';
+import secureEdge03 from '../Components/assets/images/03-secureEdge.svg';
+import secureEdge04 from '../Components/assets/images/04-secureEdge.svg';
+import secureEdge05 from '../Components/assets/images/05-secureEdge.svg';
+import success1 from '../Components/assets/images/success-1.png';
+import success2 from '../Components/assets/images/success-2.png';
+import playCircle from '../Components/assets/images/play_circle.svg';
 
 
 const Main = () => {
@@ -20,6 +28,7 @@ const Main = () => {
         autoplay: true,
       };
     return (
+        <>
         <section className='sec-main'>
             <div className='header'>
                 <div className='logo'>
@@ -61,6 +70,93 @@ const Main = () => {
                 </Slider>
             </div>
         </section>
+        <section className='sec-secureEdge'>
+            <div className='container'>
+                <div className='title-secure'>
+                    <div className='title-big-text'>How We Secure an Edge</div>
+                    <div className='title-small-text'>From content creation, distribution, tracking, to analysis, we ensure that each campaign delivers.</div>
+                </div>
+                <article className='arti-secureEdge'>
+                    <ul>
+                        <li>
+                            <div className='icon-secureEdge'>
+                                <img src={secureEdge01} alt='' />
+                            </div>
+                            <div className='text-secureEdge'>Total 360-degree Campaigns</div>
+                        </li>
+                        <li>
+                            <div className='icon-secureEdge'>
+                                <img src={secureEdge02} alt='' />
+                            </div>
+                            <div className='text-secureEdge'>Online to Offline Solutions</div>
+                        </li>
+                        <li>
+                            <div className='icon-secureEdge'>
+                                <img src={secureEdge03} alt='' />
+                            </div>
+                            <div className='text-secureEdge'>Seamless Collaboration and Support</div>
+                        </li>
+                        <li>
+                            <div className='icon-secureEdge'>
+                                <img src={secureEdge04} alt='' />
+                            </div>
+                            <div className='text-secureEdge'>Performance Tracking</div>
+                        </li>
+                        <li>
+                            <div className='icon-secureEdge'>
+                                <img src={secureEdge05} alt='' />
+                            </div>
+                            <div className='text-secureEdge'>Customized Partnerships</div>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+        <section className='sec-successful-campaign'>
+            <div className='container'>
+                <div className='title-secure'>
+                    <div className='title-big-text'>Successful Campaigns</div>
+                </div>
+                <div className='list-successful'>
+                    <ul>
+                        <li>
+                            <div className='bx-successful'>
+                                <div className='video-image'>
+                                    <img src={success1} alt='' />
+                                </div>
+                                <div className='video-name'>SRIJAN</div>
+                                <div className='icon-video'>
+                                    <img src={playCircle} alt='' />
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='bx-successful'>
+                                <div className='video-image'>
+                                    <img src={success2} alt='' />
+                                </div>
+                                <div className='video-name'>PANTALOONS</div>
+                                <div className='icon-video'>
+                                    <img src={playCircle} alt='' />
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='bx-successful'>
+                                <div className='video-image'>
+                                    <img src={success1} alt='' />
+                                </div>
+                                <div className='video-name'>ASIAN PAINTS</div>
+                                <div className='icon-video'>
+                                    <img src={playCircle} alt='' />
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        </>
     )
 }
 
