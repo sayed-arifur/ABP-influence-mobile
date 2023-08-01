a87cd89ba34f64b910c7724d85d51ada9cf6e1fb
Arifur Rahaman <sayed.rahman@abpdigital.in>
2023-08-01 16:32:50 +0530

    mobile animation secure adge

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -91,8 +91,8 @@ const Main = () => {
                 }                
                 </div>
                 <div className='title-secure' id='sec_secureEdge'>
-                    <div className='title-big-text animate fadeInLeft one'>How We Secure an Edge</div>
-                    <div className='title-small-text animate fadeInLeft two'>From content creation, distribution, tracking, to analysis, we ensure that each campaign delivers.</div>
+                    <div className='title-big-text animate fadeInUp one'>How We Secure an Edge</div>
+                    <div className='title-small-text animate fadeInUp two'>From content creation, distribution, tracking, to analysis, we ensure that each campaign delivers.</div>
                 </div>
                 <article className='arti-secureEdge'>
                     <div ref={ref} style={{fontSize: 0}}>               
@@ -103,31 +103,31 @@ const Main = () => {
                         }                
                     </div>
                     <ul id='ul_secureEdg'>
-                        <li className=' animate fadeInLeft three'>
+                        <li className=' animate fadeInUp three'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge01} alt='' />
                             </div>
                             <div className='text-secureEdge'>Total 360-degree Campaigns</div>
                         </li>
-                        <li className=' animate fadeInRight four'>
+                        <li className=' animate fadeInUp four'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge02} alt='' />
                             </div>
                             <div className='text-secureEdge'>Online to Offline Solutions</div>
                         </li>
-                        <li className=' animate fadeInLeft five'>
+                        <li className=' animate fadeInUp five'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge03} alt='' />
                             </div>
                             <div className='text-secureEdge'>Seamless Collaboration and Support</div>
                         </li>
-                        <li className=' animate fadeInRight six'>
+                        <li className=' animate fadeInUp six'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge04} alt='' />
                             </div>
                             <div className='text-secureEdge'>Performance Tracking</div>
                         </li>
-                        <li className='animate fadeInLeft seven'>
+                        <li className='animate fadeInUp seven'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge05} alt='' />
                             </div>
@@ -148,12 +148,19 @@ const Main = () => {
                     }, 100)
                 }               
 
-                <div className='title-Campaigns animate fadeInLeft two' id='title_Campaigns'>
+                <div className='title-Campaigns animate fadeInUp two' id='title_Campaigns'>
                     <div className='title-big-text'>Successful Campaigns</div>
                 </div>
-                <div className='list-successful'>
+                </div>
+                <div ref={ref} style={{fontSize: 0}}>               
+                {
+                    setTimeout(()=>{
+                        inView == true ? document.getElementById("list_successful").classList.add('show') : <></>
+                    }, 100)
+                }      
+                <div className='list-successful' id='list_successful'>
                     <ul>
-                        <li>
+                        <li className=' animate fadeInUp one'>
                             <div className='bx-successful'>
                                 <div className='video-image'>
                                     <img src={success1} alt='' />
@@ -164,7 +171,7 @@ const Main = () => {
                                 </div>
                             </div>
                         </li>
-                        <li>
+                        <li className=' animate fadeInUp two'>
                             <div className='bx-successful'>
                                 <div className='video-image'>
                                     <img src={success2} alt='' />
@@ -175,7 +182,7 @@ const Main = () => {
                                 </div>
                             </div>
                         </li>
-                        <li>
+                        <li className=' animate fadeInUp three'>
                             <div className='bx-successful'>
                                 <div className='video-image'>
                                     <img src={success1} alt='' />
