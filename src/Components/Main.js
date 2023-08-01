758e989e678c6696f3af2e4d33c93d7b4c90eb0b
Arifur Rahaman <sayed.rahman@abpdigital.in>
2023-08-01 14:55:41 +0530

    mobile animation secure adge

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -96,32 +96,39 @@ const Main = () => {
                     <div className='title-small-text animate fadeInLeft two'>From content creation, distribution, tracking, to analysis, we ensure that each campaign delivers.</div>
                 </div>
                 <article className='arti-secureEdge'>
-                    <ul>
-                        <li>
+                    <div ref={ref} style={{fontSize: 0}}>               
+                        {
+                            setTimeout(()=>{
+                                inView == true ? document.getElementById("ul_secureEdg").classList.add('show') : <></>
+                            }, 100)
+                        }                
+                    </div>
+                    <ul id='ul_secureEdg'>
+                        <li className=' animate fadeInLeft three'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge01} alt='' />
                             </div>
                             <div className='text-secureEdge'>Total 360-degree Campaigns</div>
                         </li>
-                        <li>
+                        <li className=' animate fadeInRight four'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge02} alt='' />
                             </div>
                             <div className='text-secureEdge'>Online to Offline Solutions</div>
                         </li>
-                        <li>
+                        <li className=' animate fadeInLeft five'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge03} alt='' />
                             </div>
                             <div className='text-secureEdge'>Seamless Collaboration and Support</div>
                         </li>
-                        <li>
+                        <li className=' animate fadeInRight six'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge04} alt='' />
                             </div>
                             <div className='text-secureEdge'>Performance Tracking</div>
                         </li>
-                        <li>
+                        <li className=' animate fadeInLeft seven'>
                             <div className='icon-secureEdge'>
                                 <img src={secureEdge05} alt='' />
                             </div>
