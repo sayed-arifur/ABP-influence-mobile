ab5a7b1eb65a31bcc0c814c9422f6aa959e6ec4f
Arifur Rahaman <sayed.rahman@abpdigital.in>
2023-08-03 10:02:28 +0530

    mobile animation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from './Components/Main';
 function App() {
   return (
     <div className="App">
-        <Main />
+      <Main />
     </div>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -81,18 +81,18 @@ const Main = () => {
             </div>
         </section>
         {/* sec-secureEdge */}
-        <section className='sec-secureEdge min-h-screen'>            
+        <section className='sec-secureEdge'>            
             <div className='container'> 
                 <div ref={ref} style={{fontSize: 0}}>               
-                {
-                    setTimeout(()=>{
-                        inView == true ? document.getElementById("sec_secureEdge").classList.add('show') : <></>
-                    }, 100)
-                }                
-                </div>
-                <div className='title-secure' id='sec_secureEdge'>
-                    <div className='title-big-text animate fadeInUp one'>How We Secure an Edge</div>
-                    <div className='title-small-text animate fadeInUp two'>From content creation, distribution, tracking, to analysis, we ensure that each campaign delivers.</div>
+                    {
+                        setTimeout(()=>{
+                            inView == true ? document.getElementById("sec_secureEdge").classList.add('show') : <></>
+                        }, 100)
+                    } 
+                    <div className='title-secure' id='sec_secureEdge'>
+                        <div className='title-big-text animate fadeInUp one'>How We Secure an Edge</div>
+                        <div className='title-small-text animate fadeInUp two'>From content creation, distribution, tracking, to analysis, we ensure that each campaign delivers.</div>
+                    </div>               
                 </div>
                 <article className='arti-secureEdge'>
                     <div ref={ref} style={{fontSize: 0}}>               
@@ -100,102 +100,100 @@ const Main = () => {
                             setTimeout(()=>{
                                 inView == true ? document.getElementById("ul_secureEdg").classList.add('show') : <></>
                             }, 100)
-                        }                
+                        } 
+                        <ul id='ul_secureEdg'>
+                            <li className=' animate fadeInUp three'>
+                                <div className='icon-secureEdge'>
+                                    <img src={secureEdge01} alt='' />
+                                </div>
+                                <div className='text-secureEdge'>Total 360-degree Campaigns</div>
+                            </li>
+                            <li className=' animate fadeInUp four'>
+                                <div className='icon-secureEdge'>
+                                    <img src={secureEdge02} alt='' />
+                                </div>
+                                <div className='text-secureEdge'>Online to Offline Solutions</div>
+                            </li>
+                            <li className=' animate fadeInUp five'>
+                                <div className='icon-secureEdge'>
+                                    <img src={secureEdge03} alt='' />
+                                </div>
+                                <div className='text-secureEdge'>Seamless Collaboration and Support</div>
+                            </li>
+                            <li className=' animate fadeInUp six'>
+                                <div className='icon-secureEdge'>
+                                    <img src={secureEdge04} alt='' />
+                                </div>
+                                <div className='text-secureEdge'>Performance Tracking</div>
+                            </li>
+                            <li className='animate fadeInUp seven'>
+                                <div className='icon-secureEdge'>
+                                    <img src={secureEdge05} alt='' />
+                                </div>
+                                <div className='text-secureEdge'>Customized Partnerships</div>
+                            </li>
+                        </ul>               
                     </div>
-                    <ul id='ul_secureEdg'>
-                        <li className=' animate fadeInUp three'>
-                            <div className='icon-secureEdge'>
-                                <img src={secureEdge01} alt='' />
-                            </div>
-                            <div className='text-secureEdge'>Total 360-degree Campaigns</div>
-                        </li>
-                        <li className=' animate fadeInUp four'>
-                            <div className='icon-secureEdge'>
-                                <img src={secureEdge02} alt='' />
-                            </div>
-                            <div className='text-secureEdge'>Online to Offline Solutions</div>
-                        </li>
-                        <li className=' animate fadeInUp five'>
-                            <div className='icon-secureEdge'>
-                                <img src={secureEdge03} alt='' />
-                            </div>
-                            <div className='text-secureEdge'>Seamless Collaboration and Support</div>
-                        </li>
-                        <li className=' animate fadeInUp six'>
-                            <div className='icon-secureEdge'>
-                                <img src={secureEdge04} alt='' />
-                            </div>
-                            <div className='text-secureEdge'>Performance Tracking</div>
-                        </li>
-                        <li className='animate fadeInUp seven'>
-                            <div className='icon-secureEdge'>
-                                <img src={secureEdge05} alt='' />
-                            </div>
-                            <div className='text-secureEdge'>Customized Partnerships</div>
-                        </li>
-                    </ul>
                 </article>
             </div>
         </section>
         {/* sec-successful-campaign */}
-        <section className='sec-successful-campaign min-h-screen' id='sec_successful_campaign'>
-            <div className='container'>
-                
-            <div ref={ref} style={{fontSize: 0}}>               
-                {
-                    setTimeout(()=>{
-                        inView == true ? document.getElementById("title_Campaigns").classList.add('show') : <></>
-                    }, 100)
-                }               
-
-                <div className='title-Campaigns animate fadeInUp one' id='title_Campaigns'>
-                    <div className='title-big-text'>Successful Campaigns</div>
-                </div>
+        <section className='sec-successful-campaign' id='sec_successful_campaign'>
+            <div className='container'>                
+                <div ref={ref} style={{fontSize: 0}}>               
+                    {
+                        setTimeout(()=>{
+                            inView == true ? document.getElementById("title_Campaigns").classList.add('show') : <></>
+                        }, 100)
+                    } 
+                    <div className='title-Campaigns animate fadeInUp one' id='title_Campaigns'>
+                        <div className='title-big-text'>Successful Campaigns</div>
+                    </div>
                 </div>
                 <div ref={ref} style={{fontSize: 0}}>               
-                {
-                    setTimeout(()=>{
-                        inView == true ? document.getElementById("list_successful").classList.add('show') : <></>
-                    }, 100)
-                }      
-                <div className='list-successful' id='list_successful'>
-                    <ul>
-                        <li className=' animate fadeInUp two'>
-                            <div className='bx-successful'>
-                                <div className='video-image'>
-                                    <img src={success1} alt='' />
+                    {
+                        setTimeout(()=>{
+                            inView == true ? document.getElementById("list_successful").classList.add('show') : <></>
+                        }, 100)
+                    }      
+                    <div className='list-successful' id='list_successful'>
+                        <ul>
+                            <li className=' animate fadeInUp two'>
+                                <div className='bx-successful'>
+                                    <div className='video-image'>
+                                        <img src={success1} alt='' />
+                                    </div>
+                                    <div className='video-name'>SRIJAN</div>
+                                    <div className='icon-video'>
+                                        <img src={playCircle} alt='' />
+                                    </div>
                                 </div>
-                                <div className='video-name'>SRIJAN</div>
-                                <div className='icon-video'>
-                                    <img src={playCircle} alt='' />
+                            </li>
+                            <li className=' animate fadeInUp three'>
+                                <div className='bx-successful'>
+                                    <div className='video-image'>
+                                        <img src={success2} alt='' />
+                                    </div>
+                                    <div className='video-name'>PANTALOONS</div>
+                                    <div className='icon-video'>
+                                        <img src={playCircle} alt='' />
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                        <li className=' animate fadeInUp three'>
-                            <div className='bx-successful'>
-                                <div className='video-image'>
-                                    <img src={success2} alt='' />
+                            </li>
+                            <li className=' animate fadeInUp four'>
+                                <div className='bx-successful'>
+                                    <div className='video-image'>
+                                        <img src={success1} alt='' />
+                                    </div>
+                                    <div className='video-name'>ASIAN PAINTS</div>
+                                    <div className='icon-video'>
+                                        <img src={playCircle} alt='' />
+                                    </div>
                                 </div>
-                                <div className='video-name'>PANTALOONS</div>
-                                <div className='icon-video'>
-                                    <img src={playCircle} alt='' />
-                                </div>
-                            </div>
-                        </li>
-                        <li className=' animate fadeInUp four'>
-                            <div className='bx-successful'>
-                                <div className='video-image'>
-                                    <img src={success1} alt='' />
-                                </div>
-                                <div className='video-name'>ASIAN PAINTS</div>
-                                <div className='icon-video'>
-                                    <img src={playCircle} alt='' />
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
             </div>
         </section>
         {/* sec components-creators */}
